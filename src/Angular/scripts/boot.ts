0d27e5b328ab7298d7c74484e300116091f3da7e
Rahul Potluri <potluri.s.rahul@gmail.com>
2016-06-13 14:18:05 -0500

    Git learning

--- src/Angular/scripts/boot.ts
+++ src/Angular/scripts/boot.ts
@@ -2,5 +2,4 @@
 ///<reference path="../node_modules/angular2-in-memory-web-api/typings/browser.d.ts"/> 
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app';
-
 bootstrap(AppComponent);
